3189edd513a064e556befbd8737a30031127c05a
joep1243 <31277643+joep1243@users.noreply.github.com>
2020-11-30 17:57:10 +0100

    Update AuthCheck.php

--- api/auth/AuthCheck.php
+++ api/auth/AuthCheck.php
@@ -1,4 +1,7 @@
 <?php
+include_once '../config/db.php';
+include_once '../objects/BotAccount.php';
+
 function AuthCheck($API_code){
 
     $database = new Database();
@@ -7,6 +10,12 @@ function AuthCheck($API_code){
     $BotAccount = new BotAccount($db);
 
     $pieces = explode("/", $API_code);
+
+
+    //print_r($pieces);
+    //print_r($pieces[1]);
+    
+
     
         // set BotAccount property values
         $BotAccount->username = $pieces[0];
@@ -18,10 +27,13 @@ function AuthCheck($API_code){
         // check if more than 0 record found
         if($num>0){
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-                if($API_code == $row['password']) {
+
+                if($API_code == $row['API_code']) {
                     return true;
                 }else{
+
                     return false;
+
                 }
             }   
         }else{

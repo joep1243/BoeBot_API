b629a6df952305b29474598c1742f5c61568797d
joep1243 <31277643+joep1243@users.noreply.github.com>
2020-11-30 16:20:23 +0100

    objects

---
+++ api/objects/BotLog.php
@@ -0,0 +1,127 @@
+<?php
+class BotLog{
+  
+    // database connection and table name
+    private $conn;
+    private $table_name = "temp_log";
+  
+    // object properties
+    public $ID;
+    public $BOT_ID;
+    public $log;
+    public $diagnostics;
+    public $diagnosticsname;
+    public $location;
+
+    public $logarray;
+    public $diagnosticsarray;
+    public $locationarray;
+
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+        $this->getarrays();
+    }
+
+    function getarrays(){
+        // select all query
+        $query = "SELECT log, diagnostics, location FROM " . $this->table_name . " WHERE username = '".$this->BOT_ID."'";
+    
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+    
+        // execute query
+        $stmt->execute();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $this->log = $row['log'];
+            $this->log = $row['diagnostics'];
+            $this->log = $row['location'];
+        }
+    }
+
+
+    // update location for bot
+    function UpdateLog(){
+
+        $arr = json_decode($this->logarray, TRUE);
+        $newnum = count($arr) + 1;
+        $arr[] = ['log'.$newnum.'' => $this->log];
+        $json = json_encode($arr);
+
+        // select all query
+        $query = "UPDATE " . $this->table_name . " SET log=:log  WHERE BOT_ID=:BOT_ID";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $json=htmlspecialchars(strip_tags($json));
+        $this->BOT_ID=htmlspecialchars(strip_tags($this->BOT_ID));
+
+        // bind values
+        $stmt->bindParam(":log", $json);
+        $stmt->bindParam(":BOT_ID", $this->BOT_ID);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+    }
+
+
+    // update location for bot
+    function Updatediagnostics(){
+
+        $arr = json_decode($this->diagnosticsarray, TRUE);  
+        $arr[][$this->diagnosticsname]=$this->diagnostics;   
+        $json = json_encode($arr);
+        
+        // select all query
+        $query = "UPDATE " . $this->table_name . " SET diagnostics=:diagnostics  WHERE BOT_ID=:BOT_ID";
+
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $json=htmlspecialchars(strip_tags($json));
+        $this->BOT_ID=htmlspecialchars(strip_tags($this->BOT_ID));
+
+        // bind values
+        $stmt->bindParam(":diagnostics", $json);
+        $stmt->bindParam(":BOT_ID", $this->BOT_ID);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+    }
+
+    
+     // update location for bot
+     function UpdateLocation(){
+    
+        // select all query
+        $query = "UPDATE " . $this->table_name . " SET location=:location  WHERE BOT_ID=:BOT_ID";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->location=htmlspecialchars(strip_tags($this->location));
+        $this->BOT_ID=htmlspecialchars(strip_tags($this->BOT_ID));
+    
+        // bind values
+        $stmt->bindParam(":location", $this->location);
+        $stmt->bindParam(":BOT_ID", $this->BOT_ID);
+
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
+    }
+}
+?>
--- api/objects/BotRoute.php
+++ api/objects/BotRoute.php
@@ -1,14 +1,18 @@
 <?php
-class BotCommand{
+class BotRoute{
   
     // database connection and table name
     private $conn;
-    private $table_name = "temp_command";
+    private $table_name = "bot_route";
   
     // object properties
     public $ID;
     public $BOT_ID;
-    public $API_code;
+    public $MAX_X;
+    public $MAX_Y;
+    public $start;
+    public $end;
+    public $blockade;
 
   
     // constructor with $db as database connection
@@ -16,11 +20,11 @@ class BotCommand{
         $this->conn = $db;
     }
     
-    // get command by bot id
-    function getcommand(){
+    // get route by bot id
+    function getroute(){
     
         // select all query
-        $query = "SELECT command FROM " . $this->table_name . "WHERE BOT_ID = '".$this->BOT_ID."'";
+        $query = "SELECT MAX_X, MAX_Y, start, end, blockade FROM " . $this->table_name . "WHERE BOT_ID = '".$this->BOT_ID."'";
     
         // prepare query statement
         $stmt = $this->conn->prepare($query);
